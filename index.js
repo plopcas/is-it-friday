5fd5de16232b0eae274a734000cf2db49568dbc2
Glitch (is-it-friday) <none>
2019-04-24 22:58:20 +0000

    🏍💋 Updated with Glitch

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = app => {
   app.on(['check_suite.requested', 'check_run.rerequested'], check)
   async function check (context) {
     const startTime = new Date()
-    const { head_branch: headBranch, head_sha: headSha } = context.payload.check_suite;
+    const { head_branch: headBranch, head_sha: headSha } = context.payload.check_suite
     var result = context.github.checks.create(context.repo({
       name: 'Is it Friday?',
       head_branch: headBranch,
@@ -33,10 +33,10 @@ module.exports = app => {
         output: {
           title: 'Yes! 🚨It is Friday🚨 Are you sure you want to merge?',
           summary: 'Think about your colleagues on-call, do not merge unless it is urgent, they will thank you for that.',
-          "images": [
+          images: [
             {
-              "alt": 'Begging Cat',
-              "image_url": 'https://i.pinimg.com/originals/27/69/f3/2769f3f85137a2baa2b51462627d3c24.jpg'
+              alt: 'Begging Cat',
+              image_url: 'https://i.pinimg.com/originals/27/69/f3/2769f3f85137a2baa2b51462627d3c24.jpg'
             }
           ]
         }
